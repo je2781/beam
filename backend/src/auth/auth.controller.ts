ff85835cd29330a23677cba3b0dfebd09a1a29b8
Joshua Eze <jezeadiebuo5@gmail.com>
2025-04-10 18:16:19 +0100

    local guard decorator

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -14,8 +14,8 @@ export class AuthController{
 
     }
 
-    @UseGuards(LocalAuthGuard)
-    @HttpCode(HttpStatus.OK)
+    // @UseGuards(LocalAuthGuard)
+    @HttpCode(HttpStatus.CREATED)
     @ApiOperation({ summary: 'logging in' })
     @ApiResponse({ status: 201, description: 'Authentication successful', type: User})
     @ApiBody({ type: User })
@@ -27,17 +27,20 @@ export class AuthController{
       return this.authService.login(user, response);
     }
 
-    @Post('register')
+    @HttpCode(HttpStatus.CREATED)
     @ApiOperation({ summary: 'registering new account' })
     @ApiResponse({ status: 201, description: 'Registration successful', type: User})
     @ApiBody({ type: AuthDto })
+    @Post('register')
     signup(@Body() dto: AuthDto){
         return this.authService.signup(dto);
     }
 
-    @Post('logout')
+
+    @HttpCode(HttpStatus.CREATED)
     @ApiOperation({ summary: 'ending current session' })
-    @ApiResponse({ status: 200, description: 'Logout successful', type: User})
+    @ApiResponse({ status: 201, description: 'Logout successful', type: User})
+    @Post('logout')
     logout(@Res({ passthrough: true }) res: Response){
         return this.authService.logout(res);
     }
